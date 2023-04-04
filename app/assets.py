5ad0658c856cdb21772c8105edc47078ec914d5a
Riadh <riadh.azzoun@hotmail.com>
2023-04-04 20:57:58 +0100

    Update Fixed cache problem

--- app/assets.py
+++ app/assets.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from app import app
 from flask_assets import Environment, Bundle
 
@@ -12,7 +14,7 @@ assets.register(
     Bundle(
         "css/main.css",
         "css/style.css",
-        output="gen/output.css",
+        output="gen/style.css",
         filters="cssmin",
     ),
 )
@@ -35,3 +37,8 @@ assets.register(
         filters="jsmin",
     ),
 )
+
+# Clear the Flask-Assets cache
+CACHE_DIR = os.path.join(app.static_folder, ".webassets-cache")
+if os.path.exists(CACHE_DIR):
+    shutil.rmtree(CACHE_DIR)
