d54229096ee6afa165c03f2e447435b5dad0281a
Riadh <riadh.azzoun@hotmail.com>
2023-04-05 01:33:06 +0100

    Update Fixed paths for tailwind css Added cssmin and jsmin to requirements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/templates/**/*.html",
-  "./app/templates/*.html",
-  ],
+  content: ["./templates/**/*.html", "./templates/*.html"],
   darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
